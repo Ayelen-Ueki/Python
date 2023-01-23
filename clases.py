aee0c4060590d7f2b6b702fb847f55a3ecbf7b25
Ayelen-Ueki <63622584+Ayelen-Ueki@users.noreply.github.com>
2023-01-22 23:08:25 -0300

    Initial folders and archives created

---
+++ clases.py
@@ -0,0 +1,14 @@
+class Clientes : 
+    def __init__ (self, nombre, contra, edad): 
+        self.nombre=nombre
+        self.contra=contra
+        self.edad=edad
+    
+    def __str__(self):
+        return f"se ha creado un nuevo cliente: {Clientes.nombre}"
+    
+    def registro(slef): 
+       return f"Se ha registrado exitosamente"
+    
+    def ingreso(slef): 
+       return f"Ha ingresado exitosamente"
---
+++ segundaPreEntrega/funciones.py
@@ -0,0 +1,28 @@
+def registro(): 
+    nombre = input("Por favor ingrese un Nombre de Usuario: ")
+    contra = ""
+    edad: input("Qué edad tenés?")
+
+
+    while len(Contraseña)<8:
+      contra = input("Por favor ingrese una contraseña con un mínimo de 8 caracteres: ")
+      
+    usuarioNuevo = {"NOMBRE":nombre,"PASS":contra, "EDAD": edad}
+    usuarios = []
+    usuarios.append(usuarioNuevo)
+
+    return usuarios
+
+def confirmacion(): 
+    print(f"Tu Usuario: {registro [-1] ['NOMBRE']} ha sido creado con éxito!")
+
+def ingreso():
+    nombreIngresado = input("Por favor ingrese su Nombre de Usuario: ")
+    contraseñaIngresada = input("Por favor ingrese su Contraseña: ")
+
+    for x in registro: 
+        if registro["NOMBRE"] == nombreIngresado and registro["CONTRASEÑA"] == contraseñaIngresada:
+            print("Has ingresado exitosamente")
+
+def salir():
+  print("Hasta pronto!")
